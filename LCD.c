ae96444fa5ad207b23dd5f8f3bd0b8b50b8d8dce
BHAVANA K <bhavana.kbhavanas@gmail.com>
2026-03-18 20:55:45 +0530

    Create LCD.c

---
+++ LCD.c
@@ -0,0 +1,142 @@
+//LCD using only 4 data pins
+
+#define F_CPU 8000000UL       
+#include <avr/io.h>
+#include<util/delay.h>
+#include<stdio.h>
+
+
+char buffer[10];
+void lcd_ini();
+void command(unsigned char);
+void display(unsigned char*);
+void data(unsigned char);
+
+void main(void) {
+    DDRC=0x0F;
+    DDRD=0xFF;
+    int i=0;
+    lcd_ini();
+    
+    while(1){ 
+       command(0x80); 
+       //sprintf(buffer,"%d",i++);      // to display a counter value
+       //display(buffer);
+       //_delay_ms(500);
+       
+       display("hello world");
+    }
+    return;
+}
+void lcd_ini(){
+    command(0x33);
+    _delay_ms(15);
+    command(0x32);
+    _delay_ms(15);  
+     command(0x20);
+    _delay_ms(5); 
+    command(0x28);
+    command(0x0C);
+    command(0x06);
+    command(0x01);
+    //command(0x80);
+}
+void command(unsigned char c){
+    PORTC&=~(1<<0);
+    PORTC&=~(1<<1);
+    unsigned char c_upper=(c>>4)&0x0F;
+    unsigned char c_lower=c&0x0F;
+    PORTD=c_upper;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+    _delay_ms(5);
+    PORTD=c_lower;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+}
+void display(unsigned char *ptr){
+    while(*ptr){
+        data(*ptr++);
+    }
+}
+void data(unsigned char ch)
+{
+    PORTC|=1<<0;
+    PORTC&=~(1<<1);
+    unsigned char c_upper=(ch>>4)&0x0F;
+    unsigned char c_lower=ch&0x0F;
+    PORTD=c_upper;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+    _delay_ms(5);
+    PORTD=c_lower;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+}
+
+
+
+//LCD normal method
+
+#define F_CPU 8000000UL         
+#include <avr/io.h>
+#include<util/delay.h>
+#include<stdio.h>
+
+
+char buffer[10];
+void lcd_ini();
+void command(unsigned char);
+void display(unsigned char*);
+void data(unsigned char);
+
+void main(void) {
+    DDRC=0x0F;          //
+    DDRD=0xFF;
+    int i=0;
+    lcd_ini();
+    
+    while(1){ 
+       command(0x80); 
+       sprintf(buffer,"%d",i++);
+       display(buffer);
+       _delay_ms(500);
+       
+      // command(0x14);
+       //display("hi");
+    }
+    return;
+}
+void lcd_ini(){
+    command(0x38);
+    command(0x0C);
+    command(0x06);
+    command(0x01);
+    //command(0x80);
+}
+void command(unsigned char c){
+    PORTC&=~(1<<0);
+    PORTC&=~(1<<1);
+    PORTD=c;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+}
+void display(unsigned char *ptr){
+    while(*ptr){
+        data(*ptr++);
+    }
+}
+void data(unsigned char ch)
+{
+    PORTC|=1<<0;
+    PORTC&=~(1<<1);
+    PORTD=ch;
+    PORTC|=1<<2;
+    _delay_ms(5);
+    PORTC&=~(1<<2);
+}

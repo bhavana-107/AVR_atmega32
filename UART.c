17dfc449c8ab84fa7aa290c8984f843bc1894457
BHAVANA K <bhavana.kbhavanas@gmail.com>
2026-03-18 15:12:45 +0530

    Update UART.c

--- UART.c
+++ UART.c
@@ -81,3 +81,54 @@ void display(unsigned char *ptr){
         
     }
 }
+
+
+//Task:ENtering a string and displaying it in reverse way
+
+#define F_CPU 8000000UL    
+#include <avr/io.h>
+#include<util/delay.h>
+#include<stdio.h>
+#include<string.h>
+
+void display(unsigned char*);
+
+void main(void) {
+    DDRD=0x02;
+    UCSRA=0x00;
+    UCSRB=0x18;
+    UCSRC=0x86;
+    UBRRL=51;
+    char ch[10];
+    int i;
+    while(1){
+       for(i=0;i<10;i++)
+        {
+            while(!(UCSRA&0x80));
+            ch[i]=UDR;
+            if(ch[i]=='\r'){
+                ch[i]='\0';
+                int len,j;
+                len=strlen(ch);
+                char temp;
+                for(j=0;j<len/2;j++){
+                    temp=ch[j];
+                    ch[j]=ch[len-j-1];
+                    ch[len-j-1]=temp;
+                }
+                display(ch);
+                display("\r");
+                break;
+            }
+        }
+        
+    }
+    return;
+}
+void display(unsigned char *ptr){
+    while(*ptr){
+        while(!(UCSRA&(1<<5)));
+        UDR=*ptr++;
+        
+    }
+}

72eaf7022e96bd8584f4eeac69ebce34d23c6da7
BHAVANA K <bhavana.kbhavanas@gmail.com>
2026-03-18 13:54:15 +0530

    Update Led_Blink.c

--- Led_Blink.c
+++ Led_Blink.c
@@ -22,7 +22,27 @@ void main(void) {
 //        _delay_ms(2000);
 //        PORTB&=~((1<<0)|(1<<1)|(1<<2)|(1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<7));
 //        _delay_ms(2000);
-        
+
+
+
+//Led with button
+
+#include <avr/io.h>
+#include<util/delay.h>      //for delay
+#define F_CPU 8000000UL
+
+void main(void){
+    DDRB=0x01;    //led connected to PB0
+    while(1){
+        if(PINB&0x02){      //button is in pull down configuration
+            PORTB|=(1<<0);
+        }
+        else{
+            PORTB=0;
+        }
+    }
+    return;
+}        
         
     }
     return;
